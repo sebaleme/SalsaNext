make inference possible with cuda10.1 drivers, correct runtime computation bug, correct input bugs

train/tasks/semantic/infer.py
@@ -9,8 +9,8 @@ from shutil import copyfile
 import os
 import shutil
 import __init__ as booger
-
 from tasks.semantic.modules.user import *
+
 def str2bool(v):
     if isinstance(v, bool):
        return v
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     print("log", FLAGS.log)
     print("model", FLAGS.model)
     print("Uncertainty", FLAGS.uncertainty)
-    print("Monte Carlo Sampling", FLAGS.mc)
+    print("Monte Carlo Sampling", FLAGS.monte_carlo)
     print("infering", FLAGS.split)
     print("----------\n")
     #print("Commit hash (training version): ", str(
@@ -86,7 +86,7 @@ if __name__ == '__main__':
     # open arch config file
     try:
         print("Opening arch config file from %s" % FLAGS.model)
-        ARCH = yaml.safe_load(open(FLAGS.model + "/arch_cfg.yaml", 'r'))
+        ARCH = yaml.safe_load(open(FLAGS.model + "/salsanext.yml", 'r'))
     except Exception as e:
         print(e)
         print("Error opening arch yaml file.")
@@ -139,6 +139,9 @@ if __name__ == '__main__':
         print("model folder doesnt exist! Can't infer...")
         quit()
 
+    print('__Number CUDA Devices:', torch.cuda.device_count())
+    print('__Devices')
+
     # create user and infer dataset
-    user = User(ARCH, DATA, FLAGS.dataset, FLAGS.log, FLAGS.model,FLAGS.split,FLAGS.uncertainty,FLAGS.mc)
+    user = User(ARCH, DATA, FLAGS.dataset, FLAGS.log, FLAGS.model,FLAGS.split,FLAGS.uncertainty,FLAGS.monte_carlo)
     user.infer()

train/tasks/semantic/modules/user.py
@@ -16,8 +16,11 @@ import cv2
 import os
 import numpy as np
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print(dir_path)
+
 from tasks.semantic.modules.SalsaNext import *
-from tasks.semantic.modules.SalsaNextUncertainty import *
+from tasks.semantic.modules.SalsaNextAdf import *
 from tasks.semantic.postproc.KNN import KNN
 
 
@@ -78,6 +81,7 @@ class User():
     self.gpu = False
     self.model_single = self.model
     self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(" torch.cuda.is_available(): ",  torch.cuda.is_available())
     print("Infering in device: ", self.device)
     if torch.cuda.is_available() and torch.cuda.device_count() > 0:
       cudnn.benchmark = True
@@ -225,13 +229,6 @@ class User():
         else:
             proj_output = self.model(proj_in)
             proj_argmax = proj_output[0].argmax(dim=0)
-            if torch.cuda.is_available():
-                torch.cuda.synchronize()
-            res = time.time() - end
-            print("Network seq", path_seq, "scan", path_name,
-                  "in", res, "sec")
-            end = time.time()
-            cnn.append(res)
 
             if torch.cuda.is_available():
                 torch.cuda.synchronize()